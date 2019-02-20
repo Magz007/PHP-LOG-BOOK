bbfa5d9f64770faf2bb64c95e80f3ab5ce5e5242
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-02-20 23:31:04 +0000

    new working

--- Logbook 1/Wk1ex7.php
+++ Logbook 1/Wk1ex7.php
@@ -3,8 +3,10 @@
 <?php
 	$hourlyrate	= 5.75;
 	$hoursperweek = 40;
-	$tax= 22%;
-	$gross = ($hourlyrate * $hoursperweek) - $tax;
-	echo $gross;
+	$tax= 22;
+	$gross = $hourlyrate * $hoursperweek  ;
+	$totalearn = ($tax/100)*$gross;
+	$netwages=$totalearn - $gross
+	echo $netwages;
 ?>
 </body>
