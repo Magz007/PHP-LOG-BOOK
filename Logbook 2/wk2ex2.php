43f23191134e170ad82bc591ab44e49b4cd9d63d
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-02-21 23:48:02 +0000

    new

--- Logbook 2/wk2ex2.php
+++ Logbook 2/wk2ex2.php
@@ -1,8 +1,8 @@
 <?php
 $myage = "None of your business <br/>";
 	print($myage);
-	$myage = 21;
-	print($myage . "<br/>")
-	$myage = 21.75;
-	print($myage);
+	$myage1 = 21;
+	print($myage1 . "<br/>")
+	$myage2 = 21.75;
+	print($myage2);
 ?>
