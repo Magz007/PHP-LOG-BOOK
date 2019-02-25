f125ec921ac0687b2a2090d9f9ee4ccb1b99a0fe
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-02-25 01:06:52 +0000

    wk2

---
+++ Logbook 2/wk2ex11.php
@@ -0,0 +1,19 @@
+<?php
+  $mymarks["CO450"] = 64;
+  $mymarks["CO452"] = 71;
+  $mymarks["CO454"] = 62.40;
+  $mymarks["CO451"] = 58;
+  $mymarks["CO457"] = 64;
+  $mymarks["CO455"] = 44.60;
+
+  while(list($index,$value) = each($mymarks))
+  {
+    echo "for  $index  my grade was  $value % <br/>";
+  }
+
+  $total = 0;
+  $total = $total + $mymarks[$index];
+  $average = $total /  6;
+ echo $average;
+
+?>
