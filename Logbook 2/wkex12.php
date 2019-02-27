a98f90f6ba530fc12d4d0052d2b29938c7336af6
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-02-27 00:26:22 +0000

    up

--- Logbook 2/wkex12.php
+++ Logbook 2/wkex12.php
@@ -4,25 +4,30 @@
   $topmodules[2] = "Programming 2";
   $topmodules[3] = "Web Application Development";
   $topmodules[4] = "Software Engineering";
-  $mymarks["year 1"] = 55;
-  $mymarks["year 2"] = 65;
-  $mymarks["year 3"] = 75;
-  $mymarks["year 4"] = 65;
-  $mymarks["year 5"] = 75;
 
 ?>
+<?php
+$mymarks["year 1"] = 55;
+$mymarks["year 2"] = 65;
+$mymarks["year 3"] = 75;
+$mymarks["year 4"] = 65;
+$mymarks["year 5"] = 75;
+  ?>
+
 <head>
 <title>Data in table</title>
 </head>
 <body>
 <table border=1 align="center">
 <tr><th>Index</th><th>Subject</th><th>Mark</th><th>Year</tr>
+
 <?php
   for($count = 0;$count < 5;$count++)
   {
-    echo "<tr> <td>$count</td> <td>$topmodules[$count]</td> <td>$mymarks[$value]</td> </tr>";
+    echo "<tr> <td>$count</td> <td>$topmodules[$count]</td> <td>$topmarks[$count]</td> </tr>";
   }
 ?>
+
 </table>
 </body>
 </html>
