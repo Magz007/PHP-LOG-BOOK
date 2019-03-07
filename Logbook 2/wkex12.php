85345c5d15710ebaf793b7472e7bb82695c08e06
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-03-07 19:36:51 +0000

    new

--- Logbook 2/wkex12.php
+++ Logbook 2/wkex12.php
@@ -1,16 +1,5 @@
 <?php
-  $topmodules[0] = "Open Source Systems ";
-  $topmodules[1] = "Programming 1";
-  $topmodules[2] = "Programming 2";
-  $topmodules[3] = "Web Application Development";
-  $topmodules[4] = "Software Engineering";
-  ?>
-  <?php
-$mymarks["year 1"] = 55;
-$mymarks["year 2"] = 65;
-$mymarks["year 3"] = 75;
-$mymarks["year 4"] = 65;
-$mymarks["year 5"] = 75;
+
 
 $data = array (
   array(
@@ -59,7 +48,7 @@ array(
 <?php
   for($count = 0;$count < count($data);$count++)
   {
-    echo "<tr> <td>{$data[$count]['code']}</td><td>{$data[$count]['name']}</td><td>{$data[$count]['yr']}</td><td>{$data[$count]['grade']}</td>  </tr>";
+    echo "<tr> <td>{$data[$count]['code']}</td><td>{$data[$count]['name']}</td><td>{$data[$count]['yr']}</td><td>{$data[$count]['grade']}</td></tr>";
 
   }
 ?>
