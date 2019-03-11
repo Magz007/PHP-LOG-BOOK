896402f5caca46a48c0f9fbe67d5a534dad0a6c6
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-03-11 22:41:24 +0000

    new

--- Logbook 3/wk3ex1action.php
+++ Logbook 3/wk3ex1action.php
@@ -1,14 +1,12 @@
 <html>
 <head>
-	<title>Response to form</title>
+<title>Response to form</title>
 </head>
 <body>
 <?php
-
-	echo "Your name is $_POST[txtname] <br/>";
-	echo "Your gender is $_POST[radsex] <br/> ";
-	echo "Your occupation is $_POST[seloccupation] <br/>";
-
+	echo "Your name is $_POST[txtname] ";
+	echo "Your gender is $_POST[radsex]  ";
+	echo "Your occupation is $_POST[seloccupation] ";
 ?>
 </body>
 </html>
