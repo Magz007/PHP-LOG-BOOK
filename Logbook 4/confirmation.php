3f0c7edfc270dc0e06c960135a3820f6ddc17f47
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-02 17:22:41 +0100

    new

--- Logbook 4/confirmation.php
+++ Logbook 4/confirmation.php
@@ -1,4 +1,7 @@
 <?php
-   echo "<h2> Your order qty is $_GET[selqty] </h2></br>";
-   echo "<h2> and the selected colour is $_GET[selcolour].</h2>";
+<h2>
+   echo "Your order qty is $_GET[selqty]</br>" ;
+   echo " and the selected colour is $_GET[selcolour]." ;
+</h2>
+
 ?>
