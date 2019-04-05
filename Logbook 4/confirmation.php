5addfadf38bca93dd1d9749bba10ba1bf99ab9dd
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-05 20:56:28 +0100

    new

--- Logbook 4/confirmation.php
+++ Logbook 4/confirmation.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
    echo "Your order qty is $_POST[hdselqty]" ;
-   echo " and the selected colour is". $_session["selcolour"]. ;
+   echo " and the selected colour is $_session["selcolour"]. ;
 ?>
