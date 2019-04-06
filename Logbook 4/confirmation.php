43e49745d9a38d95889ec9302a67244a86c3138e
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-06 22:54:00 +0100

    yes

--- Logbook 4/confirmation.php
+++ Logbook 4/confirmation.php
@@ -2,7 +2,11 @@
 session_start();
    echo "Your order qty is $_POST[hdselqty]" ;
    echo " and the selected colour is $_POST[selcolour]." ;
+ $total= 0
+      While(hdselqty ==15.79 )
+     {
+              $total = hdselqty * 15.79;
+    }
 
-   echo " Total price is  $_POST[ $value]";
-
+   echo "Total Price $total";
 ?>
