359a9f5c7f54212567edec281c439166f6c4a9db
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-06 22:55:52 +0100

    new

--- Logbook 4/confirmation.php
+++ Logbook 4/confirmation.php
@@ -3,10 +3,9 @@ session_start();
    echo "Your order qty is $_POST[hdselqty]" ;
    echo " and the selected colour is $_POST[selcolour]." ;
  $total= 0
-      While(hdselqty == "15.79" )
-     {
-              $total = hdselqty * 15.79;
-    }
+
+                 $total = hdselqty * 15.79;
+    
 
    echo "Total Price $total";
 ?>
