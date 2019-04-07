061368a15adcef7c6ed5c2f38aa9f68d38c4b27a
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-07 14:13:27 +0100

    new

--- Logbook 4/confirmation.php
+++ Logbook 4/confirmation.php
@@ -4,4 +4,25 @@ session_start();
    echo " and the selected colour is $_POST[selcolour].</br>" ;
   echo "The  Price  is for each is  $_POST[hdprice].<br/>";
   echo "The Total price to pay is   $_POST[hdtotal]";
+
+  $total= 0
+
+  if ( $selsize == [small]  )
+  {
+    echo "Public transport is your best option. <br/>";
+  }
+  elseif ($selsize == [Medium] )
+  {
+    echo "If you get caught say your grandmother was driving. <br/>";
+  }
+  elseif($selsize ==Large )
+  {
+    echo "There is no need to worry about the speed limit.<br/>";
+  }
+  elseif ($selsize == Extra Large)
+  {
+    echo "There is no need to worry about the speed limit.<br/>";
+  }
+
+
 ?>
--- Logbook 4/selectcolour.php
+++ Logbook 4/selectcolour.php
@@ -22,29 +22,6 @@
         <br/>
         <br/>
 <input type="hidden" name="hdselqty" value="<?php echo $_POST["selqty"]?>"/>
-
-<?php
-$total= 0
-
-if ( $selsize == [small]  )
-{
-  echo "Public transport is your best option. <br/>";
-}
-elseif ($selsize == [Medium] )
-{
-  echo "If you get caught say your grandmother was driving. <br/>";
-}
-elseif($selsize ==Large )
-{
-  echo "There is no need to worry about the speed limit.<br/>";
-}
-elseif ($selsize == Extra Large)
-{
-  echo "There is no need to worry about the speed limit.<br/>";
-}
-
-
-?>
 <input type="hidden" name="hdsize" value="<?php echo $_POST["size"]?>"/>
 <input type="hidden" name="hdprice" value="<?php echo $_POST["price"]?>"/>
 <input type="submit" value="Buy"/>
