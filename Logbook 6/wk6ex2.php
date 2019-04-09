73d631bc827c6ef34b20534e6f5f57ef245f4dac
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-10 00:35:55 +0100

    result

--- Logbook 6/wk6ex2.php
+++ Logbook 6/wk6ex2.php
@@ -3,16 +3,22 @@
 	// Connect to server and select database
 	$sql = "SELECT * from test";
 	// Execute sql statement
-
+$link = mysqli_connect(‘localhost’,’login’,’pwd’, ‘my_database’);
+$result = mysqli_query($link,$sql);
 ?>
 <html>
 <body>
 
 <?php
-while ($row = mysqli_fetch_assoc($mysql_test))
+while ($row = mysqli_fetch_assoc($result))
 {
       echo "<a href=\"wk6ex2action.php? id=$row[name]\">$row[name]</a></br>";
 }
+
+mysqli_free_result($result);
+mysqli_close($link);
+
+
 ?>
 </body>
 </html>
