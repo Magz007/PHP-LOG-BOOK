62d1e10adebcdbb255fcc65fc6dd1eed661c94fc
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-10 00:03:00 +0100

    new

---
+++ Logbook 6/wk6ex2.php
@@ -0,0 +1,18 @@
+<?php
+
+	// Connect to server and select database
+	$sql = "SELECT * from test";
+	// Execute sql statement
+
+?>
+<html>
+<body>
+
+<?php
+while ($row = mysqli_fetch_assoc($result))
+{
+      echo "<a href=\"wk6ex2action.php?id=$row[name]\">$row[name]</a></br>";
+}
+?>
+</body>
+</html>
