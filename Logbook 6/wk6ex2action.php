722ef3699c407b89883e3e32e4352f6db9fb231e
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-11 21:38:05 +0100

    new

--- Logbook 6/wk6ex2action.php
+++ Logbook 6/wk6ex2action.php
@@ -2,23 +2,26 @@
 
 	// Connect to server and select database
 
-	$sql = "SELECT * from test where name = '$_GET[id]";
+$sql = "SELECT * from test where name = $_GET[id]";
 	// Execute query
-	$row = mysqli_fetch_assoc($result);
+$row = mysqli_fetch_assoc($result);
+
 ?>
 <html>
 <body>
+
 <form action=""  method="post">
 
 	Name :
-	<input type=text name=txtname value="<? echo $row[name] ?>" readonly />
-	</br>
+	<input type=text name=txtname value="<? echo $row[name] ?>" readonly </br> />
+
 	Phone number :
-	<input type=text name=txttelno value="<? echo $row[phone_number] ?>" />
-	</br>
+	<input type=text name=txttelno value="<? echo $row[phone_number] ?>" </br>/>
+
 	Email :
-	<input type=text name=txtemail value="<? echo $row[email] ?>" />
-	</br>
+	<input type=text name=txtemail value="<? echo $row[email] ?>" 	</br>/>
+
 	<input type=submit name=btnsubmit value="save"/>
+	
 </form>
 </body>
