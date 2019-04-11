de7396d8cc8d0a221addfcfe5fb8b7f4e98f6307
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-11 22:10:17 +0100

    new

--- Logbook 6/wk6ex2action.php
+++ Logbook 6/wk6ex2action.php
@@ -1,20 +1,21 @@
 <?php
-
 	// Connect to server and select database
 $link = mysqli_connect("localhost","root","","db1_gwalke01");
-
-$sql = "SELECT * from test where name = $_GET[id]";
-$result = mysqli_query($link,$sql);
-$row = mysqli_fetch_assoc($result);
-while ($row = mysqli_fetch_row($result))
-{
-echo " $row[0] $row[1] <br/>";
-}
 
 ?>
 <html>
 <body>
+	<?php
+	$sql = "SELECT * from test where name = $_GET[id]";
+	$result = mysqli_query($link,$sql);
+	$row = mysqli_fetch_assoc($result);
 
+	while ($row = mysqli_fetch_row($result))
+	{
+	echo " $row[0] $row[1] <br/>";
+	}
+
+	?>
 <form action=""  method="post">
 
 	Name :
