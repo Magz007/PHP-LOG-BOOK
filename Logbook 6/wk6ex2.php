2b30da7d9edec3ffef4be1c55ca08c5c3d1e56e2
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-11 20:16:28 +0100

    NEW

--- Logbook 6/wk6ex2.php
+++ Logbook 6/wk6ex2.php
@@ -4,7 +4,15 @@
 
 	$sql = "SELECT * from test";
 	$link = mysqli_connect("localhost","root","","db1_gwalke01");
-	$result = mysqli_query($link,$sql);
+
+	if(!$link)
+	{
+		echo "Error: Unable to connect to MySQL."; . PHP_EOL;
+	}
+
+	echo "Success: A proper connention to MySQL was made."; .PHP_EOL;
+
+
 	// Execute sql statement
 
 ?>
@@ -12,9 +20,10 @@
 <body>
 
 <?php
+$result = mysqli_query($link,$sql);
 while ($row = mysqli_fetch_assoc($result))
 {
-      echo "<a href=\"wk6ex2action.php?id=$row[name]\"</a> </br>";
+      echo "<a href=\"wk6ex2action.php?id=$row[name]\" >$row[name] </a> </br>";
 }
 
 mysqli_free_result($result);
