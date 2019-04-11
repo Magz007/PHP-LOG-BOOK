843221b083873f120fe8a3cb7fc6988727ba65be
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-11 22:07:37 +0100

    NEW

--- Logbook 6/wk6ex2.php
+++ Logbook 6/wk6ex2.php
@@ -1,8 +1,5 @@
 <?php
-
 	// Connect to server and select database
-
-
 	$link = mysqli_connect("localhost","root","","db1_gwalke01");
 
 	if(!$link)
@@ -11,11 +8,7 @@
 		exit;
 	}
 	echo "Success: A proper connention to MySQL was made.</br>";
-
-
-
 	// Execute sql statement
-
 ?>
 <html>
 <body>
@@ -27,9 +20,7 @@ $result = mysqli_query($link,$sql);
 while ($row = mysqli_fetch_assoc($result))
 {
       echo "<a href=\"wk6ex2action.php?id= $row[name]\" >$row[name]   </a> </br>";
-
 }
-
 mysqli_free_result($result);
 mysqli_close($link);
 ?>
--- Logbook 6/wk6ex2action.php
+++ Logbook 6/wk6ex2action.php
@@ -6,6 +6,10 @@ $link = mysqli_connect("localhost","root","","db1_gwalke01");
 $sql = "SELECT * from test where name = $_GET[id]";
 $result = mysqli_query($link,$sql);
 $row = mysqli_fetch_assoc($result);
+while ($row = mysqli_fetch_row($result))
+{
+echo " $row[0] $row[1] <br/>";
+}
 
 ?>
 <html>
