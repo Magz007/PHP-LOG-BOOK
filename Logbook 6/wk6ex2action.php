d7bdc30dcca5c1d6a115547be5dc8759c8b8d33b
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-11 22:32:40 +0100

    new

--- Logbook 6/wk6ex2action.php
+++ Logbook 6/wk6ex2action.php
@@ -14,7 +14,7 @@
 	// Execute query
   $result = mysqli_query($link,$sql);
 	$row = mysqli_fetch_assoc($result);
-	
+
 	mysqli_free_result($result);
 	mysqli_close($link);
 
@@ -25,13 +25,13 @@
 <form action="" method="post">
 
  	Name :
-	<input type=text name=txtname value="<? echo $row[name] ?>" readonly />
+	<input type=text name=txtname value="<? echo $row["name"] ?>" readonly />
 	</br>
 	Phone number :
-	<input type=text name=txttelno value="<? echo $row[phone_number] ?>" />
+	<input type=text name=txttelno value="<? echo $row["phone_number"] ?>" />
 	</br>
 	Email :
-	<input type=text name=txtemail value="<? echo $row[email] ?>" />
+	<input type=text name=txtemail value="<? echo $row["email"] ?>" />
 	</br>
 	<input type=submit name=btnsubmit value="save"/>
 </form>
