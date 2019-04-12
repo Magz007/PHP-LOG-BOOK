5d592c6307d1b316ed050886c2d9cfdcb7689dfd
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-12 10:23:55 +0100

    new

--- Logbook 6/wk6ex2action.php
+++ Logbook 6/wk6ex2action.php
@@ -10,7 +10,7 @@
 	}
 	echo "Success: A proper connention to MySQL was made.</br>";
 
-	$sql = "SELECT * from test where name = '$_GET[id]' ";
+	$sql = "SELECT * from test  ";
 	// Execute query
   $result = mysqli_query($link,$sql);
 	$row = mysqli_fetch_assoc($result);
