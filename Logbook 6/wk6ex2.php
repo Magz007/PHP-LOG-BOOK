326f107d226d6b9c02c3b9174fbe414a8945ca51
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-13 15:37:52 +0100

    new

--- Logbook 6/wk6ex2.php
+++ Logbook 6/wk6ex2.php
@@ -19,7 +19,7 @@ $result = mysqli_query($link,$sql);
 
 while ($row = mysqli_fetch_assoc($result))
 {
-      echo  “$row[0] <br/>”;
+      echo  “$row[2] <br/>”;
 }
 mysqli_free_result($result);
 mysqli_close($link);
