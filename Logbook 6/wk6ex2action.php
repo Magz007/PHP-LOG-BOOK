7bbb8588716807be2d639daaa5160376cfcd96f4
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-14 00:26:05 +0100

    new

--- Logbook 6/wk6ex2action.php
+++ Logbook 6/wk6ex2action.php
@@ -29,21 +29,20 @@
 	</br>
 	<input type=submit name=btnsubmit value="save"/>
 
-	<?php
-	if (isset($_POST['save']))
+		<?php
+	if (isset($_POST[save]))
 	{
-
 	$number=$_POST['phone_Number'];
 	$mail=$_POST['email'];
-	$file=fopen ("wk6ex2action.php");
+
+	$file=fopen ("updated ", "a");
+
 	$s= $number. ",".$mail."\n";
+
 	fputs($file,$s);
 	fclose($file);
-
 	}
-
-
-	 ?>
+	   ?>
 
 
 </form>
--- Logbook 6/wk6ex3action.php
+++ Logbook 6/wk6ex3action.php
@@ -1,16 +1,11 @@
 <?php
 if (isset($_POST['save']))
 {
+$number=$_POST['phone_Number'];
+$mail=$_POST['email'];
 $file=fopen ("wk6ex2action.php");
-
-
+$s= $number. ",".$mail."\n";
+fputs($file,$s);
+fclose($file);
 }
-
-
-
-
-
-
-
-
- ?>
+   ?>
