f5635ef3cfc6acbd0359b7bbadb8bea5cbf7f1b8
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-15 16:48:32 +0100

    new

--- Logbook 6/wk6ex3action.php
+++ Logbook 6/wk6ex3action.php
@@ -22,9 +22,9 @@ $fetchQuery = mysqli_query($link,$sql);
  if(isset($_POST ['submitDeleteBtn']))
  {
    $key= $_POST['btndelete'];
-   $check=mysqli_query($link,"SELECT * from test where id='$key" );
+   $result=mysqli_query($link,"SELECT * from test where id='$key" );
 
-   if (mysqli_num_rows( $check))
+   if (mysqli_num_rows( $result))
    {
      $queryDelete=mysqli_query($link,"DELETE from  test where id = '$key'");
    }
