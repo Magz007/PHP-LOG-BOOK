a1a82609b32028efab8b6f6d0ef4d63fe6bd837c
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-16 16:02:06 +0100

    new

--- Logbook 6/wk6ex3action.php
+++ Logbook 6/wk6ex3action.php
@@ -15,17 +15,13 @@ $fetchQuery = mysqli_query($link,$sql);
  ?>
 <html>
 <head> Delete Record </head>
-
 <body style="padding-top: 100px;" >
 <div class= "container">
-
   <?php
  if(isset($_POST ['submitDeleteBtn']))
  {
    $key= $_POST['btndelete'];
-
    $result=mysqli_query($link,"SELECT * from test where id='$key'");
-
    if (mysqli_num_rows($result)>0)
    {
      $queryDelete=mysqli_query($link,"DELETE * from test where id = '$key'");
@@ -42,22 +38,18 @@ $fetchQuery = mysqli_query($link,$sql);
           <th> ID </th>
   </tr>
   <?php
-
 $sr=1;
 while($row= mysqli_fetch_array($fetchQuery))
 {
   ?>
-
   <tr>
     <form action="" method= "post" role = "form">
   <td><?php echo $sr ;?> </td>
   <td><?php echo $row['name'] ;?> </td>
     <td><?php echo $row['email'] ;?> </td>
       <td><?php echo $row['phone_Number'] ;?> </td>
-
         <td> <input type= "checkbox" name= "btndelete" value=<?php echo $row['ID'] ;?> required></td>
         <td> <input type= "submit" name="submitDeleteBtn" class-= "btn btn-info">  </td>
-
      </form>
 </tr>
 <?php $sr ++ ;}
