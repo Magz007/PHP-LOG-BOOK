5e5aa0294bda483db73a194c948ff780146ef84a
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-04-17 21:44:55 +0100

    new

--- Logbook 6/wk6ex6.php
+++ Logbook 6/wk6ex6.php
@@ -1,6 +1,6 @@
 <?php
 	include("myfunctions.inc");
 	html_header("My second function demo");
-	echo "I pay £ " . calculatetax(900) . "tax";
+	echo "I pay £". calculatetax(900) . " tax . My Allowance  is £" .taxallowance(300) ;
 	html_footer();
 ?>
